bb2de9c6c2ca4a702c01826ff45e6075ca6edfa1
Jang2723 <jangsb46@gmail.com>
2023-12-21 17:51:25 +0900

    12/21

--- user.sql
+++ user.sql
@@ -1,4 +1,39 @@
-SELECT * FROM user;
+DROP TABLE IF EXISTS user;
 
--- 1번 사용자의 first_name 을 Alexander로 바꾼다.
--- UPDATE user SET first_name = 'Alexander' WHERE id = 1;
+CREATE TABLE user(
+                     id integer primary key autoincrement,
+                     username text unique,
+                     password text,
+                     first_name text,
+                     last_name text,
+                     email text
+);
+
+INSERT INTO user(username, password, first_name, last_name, email)
+VALUES
+    ('alex', 'aaaa', 'Alexander', 'Rodriguez', 'a.rod@gmail.com'),
+    ('brad', 'bbb', 'Bradley', 'Snyder', 'bradS@gmail.com'),
+    ('chad', 'ccccc', 'Chadwick', 'Bradford', 'moneyball@gmail.com');
+
+-- SQL을 작성할건데
+-- 사용자가 아이디랑 비밀번호를 제출하면
+-- 해당 아이디를 가진 행의 데이터의 비밀번호가 제출한 비밀번호와 일치하는지를 비교
+-- alex, 1234
+SELECT *
+FROM user
+WHERE
+    username = '%s' AND password = '%s';
+
+-- SQL Injection
+-- username: alex
+-- password; 1' OR '1' = '1
+SELECT *
+FROM user
+WHERE
+     username = 'alex' AND password = '1' OR '1' = '1';
+
+
+INSERT INTO article(title,content) VALUES ('%s','%s');
+-- content:  'a'); DROP TABLE article; --'
+INSERT INTO article(title, content) VALUES ('%s','a'); DROP TABLE article; --'
+
